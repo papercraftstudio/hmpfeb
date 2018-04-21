5aa0ba69c05088d534954ca186397dc9c8effe53
Ricky Afdita Adib Priliandana <23april96@gmail.com>
2018-04-21 13:01:52 +0700

    fix center element (deprecated in html5) passing error message to login page

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,46 +1,44 @@
 <?php
-class Login extends CI_Controller
-{
 
-  function __construct()
-  {
-    parent::__construct();
-    $this->load->model('m_login');
-  }
+class Login extends CI_Controller {
 
-  function index()
-  { 
-    $data['title'] = "Login";
-    $this->load->view('v_login',$data);
-  }
-
-  function login_action()
-  {
-    $username = $this->input->post('username');
-    $password = $this->input->post('pass');
-    $table = 'm_user';
-    $where = array(
-        'M_UserLogin' => $username,
-        'M_UserPassword' => md5($password)
-        );
-    $cek = $this->m_login->cek_login($username,$password)->num_rows();
-    
-    if($cek > 0){
-      $data_session = array(
-        'nama' => $username,
-        'status' => "login"
-        );
-      $this->session->set_userdata($data_session);
-      redirect(base_url("admin"));
-    }else{
-      echo "Username dan password salah !";
+    function __construct() {
+        parent::__construct();
+        $this->load->model('m_login');
     }
-  }
 
-  function logout(){
-		$this->session->sess_destroy();
-		redirect(base_url('login'));
-	}
+    function index() {
+        $data['title'] = "Login";
+        $this->load->view('v_login', $data);
+    }
 
-  
-}?>
+    function login_action() {
+        $username = $this->input->post('username');
+        $password = $this->input->post('pass');
+        $table = 'm_user';
+        $where = array(
+            'M_UserLogin' => $username,
+            'M_UserPassword' => md5($password)
+        );
+        $cek = $this->m_login->cek_login($table, $where)->num_rows();
+
+        if ($cek > 0) {
+            $data_session = array(
+                'nama' => $username,
+                'status' => "login"
+            );
+            $this->session->set_userdata($data_session);
+            redirect(base_url("admin"));
+        } else {
+            $data["error"] = "Username dan password salah!";
+            $this->load->view('v_login', $data);
+        }
+    }
+
+    function logout() {
+        $this->session->sess_destroy();
+        redirect(base_url('login'));
+    }
+
+
+} ?>
--- application/views/v_login.php
+++ application/views/v_login.php
@@ -1,37 +1,42 @@
+<!DOCTYPE html>
 <html>
 
 <head>
-    <title>
-        <?= isset($title) ? $title:'Untitled';?>
-    </title>
-    <link rel='stylesheet prefetch' href='<?=base_url();?>assets/bootstrap/css/bootstrap.min.css'>
-    <link rel='stylesheet prefetch' href='<?=base_url();?>assets/font-awesome/css/font-awesome.min.css'>
-    <style>
-        .pad{
-            margin-top: 10px;
-            text-align: center;
-        }
-    </style>
+    <title><?= isset($title) ? $title : 'Untitled'; ?></title>
+    <link rel='stylesheet prefetch' href='<?= base_url(); ?>assets/bootstrap/css/bootstrap.min.css'>
+    <link rel='stylesheet prefetch' href='<?= base_url(); ?>assets/font-awesome/css/font-awesome.min.css'>
 </head>
 
 <body>
-    <center>
-        <div class="col-lg-4" style="margin-top: 80px">
-            <div class="jumbotron">
-                <form method="POST" action="<?= base_url('login/login_action'); ?>">
-                    <input class="form-control pad" type="text" name="username" id="username" placeholder="Username" />
-                    <input class="form-control pad" type="password" name="pass" id="pass" placeholder="Password" />
-                    <button class="btn btn-primary pad" type="submit">LOGIN </button>
-                </form>
-            </div>
+<div class="row">
+    <div class="col-lg-4"></div>
+    <div class="col-lg-4" style="margin-top: 80px">
+        <div class="jumbotron text-center">
+            <?php if (isset($error)) { ?>
+                <div class="alert alert-danger">
+                    <strong>Oops!</strong> <?= $error ?>
+                </div>
+            <?php } ?>
+            <form method="POST" action="<?= base_url('login/login_action'); ?>">
+                <div class="form-group">
+                    <input class="form-control text-center" type="text" name="username" id="username" placeholder="Username"/>
+                </div>
+                <div class="form-group">
+                    <input class="form-control text-center" type="password" name="pass" id="pass" placeholder="Password"/>
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-primary" type="submit">LOGIN</button>
+                </div>
+            </form>
         </div>
-    </center>
+    </div>
+    <div class="col-lg-4"></div>
+</div>
 
-
-    <!-- Script -->
-    <script src='<?=base_url();?>assets/jquery/jquery-3.2.1.min.js'></script>
-    <script src='<?=base_url();?>assets/bootstrap/js/bootstrap.min.js'></script>
-    <script src='<?=base_url();?>assets/bootstrap/js/bootstrap.bundle.min.js'></script>
+<!-- Script -->
+<script src='<?= base_url(); ?>assets/jquery/jquery-3.2.1.min.js'></script>
+<script src='<?= base_url(); ?>assets/bootstrap/js/bootstrap.min.js'></script>
+<script src='<?= base_url(); ?>assets/bootstrap/js/bootstrap.bundle.min.js'></script>
 </body>
 
 </html>
