270583e8fb02a1b24a4aecb01ad2ffdf1b22eb81
dytra <vancielnugraha@gmail.com>
2018-04-24 18:37:18 +0700

    delete insert_page.php

--- application/views/page/edit_page.php
+++ application/views/page/edit_page.php
@@ -2,13 +2,14 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Edit Page | Admin</title>
     <script src="<?= base_url() ?>assets/tinymce/jquery.tinymce.min.js"></script>
     <script src="<?= base_url() ?>assets/tinymce/tinymce.min.js"></script>
     <script>tinymce.init({ selector:'textarea' });</script>
   </head>
   <body>
-    insert page
+    
+    <p>Judul : <input type="text" name="judul" placeholder="Judul Halaman"> </p>
     <textarea name="name" rows="8" cols="80"></textarea>
   </body>
 </html>
