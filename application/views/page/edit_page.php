fe6fdd92f4bd6dc1f0ae9bb085318c897f4e736a
dytra <vancielnugraha@gmail.com>
2018-04-24 20:16:35 +0700

    edit page form using form helper

--- application/views/page/edit_page.php
+++ application/views/page/edit_page.php
@@ -8,8 +8,31 @@
     <script>tinymce.init({ selector:'textarea' });</script>
   </head>
   <body>
-    
-    <p>Judul : <input type="text" name="judul" placeholder="Judul Halaman"> </p>
-    <textarea name="name" rows="8" cols="80"></textarea>
+    <!-- <form action="http://localhost/hmpfeb/page/editted" method="post">
+      <p>Judul : <input type="text" name="title" placeholder="Judul Halaman"> </p>
+      <textarea name="content" rows="8" cols="80"></textarea>
+      <p><input type="submit" name="update" value="Update"></p>
+    </form> -->
+
+    <?php
+
+    echo form_open('page/editted');
+
+    //Show page title field
+    echo form_label('Judul Halaman : ','title');
+    $data = array(
+      'name' => 'title',
+      'placeholder' => 'Judul Halaman',
+      'class' => 'title'
+    );
+    echo form_input($data);
+
+    //Show page content field
+    $data = array(
+      'name' => 'content'
+    );
+    echo form_textarea($data);
+
+    ?>
   </body>
 </html>
