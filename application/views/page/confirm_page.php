95ab9b18a502cd825c0ad741ae82c8bd77649a62
dytra <vancielnugraha@gmail.com>
2018-04-24 20:15:51 +0700

    add confirm_page

---
+++ application/views/page/confirm_page.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <p>Judul : <?= $title ?></p>
+    <p>Content : <?= $content?> </p>
+  </body>
+</html>
