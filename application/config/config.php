2c62bfa86aaf237edbf8606b2c62fa42868ebd11
Yafia Samuel Halim <yafiasamuelhalim@gmail.com>
2018-05-02 22:26:02 +0700

    Repair config

--- application/config/config.php
+++ application/config/config.php
@@ -23,11 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-<<<<<<< HEAD
 $config['base_url'] = 'http://localhost/HMP/';
-=======
-$config['base_url'] = 'http://economics-uksw/';
->>>>>>> 7bd684dc8fa20c6ec472c781aa026f44354e5f25
 
 /*
 |--------------------------------------------------------------------------
