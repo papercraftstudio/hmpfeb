a9c7ad6c823e6a32fbdb26763eef52712f3aa84a
Ricky Afdita Adib Priliandana <23april96@gmail.com>
2018-05-04 16:19:47 +0700

    fix & optimize login view

--- application/views/header.php
+++ application/views/header.php
@@ -1,128 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
-
-
-<!-- Mirrored from www.ansonika.com/findoctor/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 24 Apr 2018 14:17:49 GMT -->
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="Find easily a doctor and book online an appointment">
-	<meta name="author" content="Ansonika">
-	<title><?= isset($title) ? $title:'Untitled';?></title>
-
-	<!-- Favicons-->
-	<link rel="shortcut icon" href="<?= base_url() ?>img/favicon.ico" type="image/x-icon">
-	<link rel="apple-touch-icon" type="image/x-icon" href="<?= base_url() ?>assets/modal/img/apple-touch-icon-57x57-precomposed.png">
-	<link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="<?= base_url() ?>assets/modal/img/apple-touch-icon-72x72-precomposed.png">
-	<link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="<?= base_url() ?>assets/modal/img/apple-touch-icon-114x114-precomposed.png">
-	<link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="<?= base_url() ?>assets/modal/img/apple-touch-icon-144x144-precomposed.png">
-
-	<!-- BASE CSS -->
-	
-	<link href="<?= base_url() ?>assets/modal/css/style.css" rel="stylesheet">
-	<link href="<?= base_url() ?>assets/modal/css/menu.css" rel="stylesheet">
-	<link href="<?= base_url() ?>assets/modal/css/vendors.css" rel="stylesheet">
-	<link href="<?= base_url() ?>assets/modal/css/icon_fonts/css/all_icons_min.css" rel="stylesheet">
-
-
-	<link rel='stylesheet prefetch' href='<?=base_url();?>assets/bootstrap/css/bootstrap.min.css'>
-    <link rel='stylesheet prefetch' href='<?=base_url();?>assets/font-awesome/css/font-awesome.min.css'>
-
-    
-	<!-- YOUR CUSTOM CSS -->
-	<!-- <link href="css/custom.css" rel="stylesheet"> -->
-
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Find easily a doctor and book online an appointment">
+    <meta name="author" content="Ansonika">
+    <title><?= isset($title) ? $title : 'Untitled'; ?></title>
+    <link rel='stylesheet' href='<?= base_url() ?>assets/bootstrap/css/bootstrap.min.css'>
+    <link rel='stylesheet' href='<?= base_url() ?>assets/font-awesome/css/font-awesome.min.css'>
+    <link rel="stylesheet" href="<?= base_url() ?>assets/modal/css/style.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/modal/css/menu.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/modal/css/vendors.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/modal/css/icon_fonts/css/all_icons_min.css">
 </head>
-
 <body>
-
-	<div id="preloader" class="Fixed">
-		<div data-loader="circle-side"></div>
-	</div>
-	<!-- /Preload-->
-	
-	<div id="page">		
-	<header class="header_sticky">	
-		<a href="#menu" class="btn_mobile">
-			<div class="hamburger hamburger--spin" id="hamburger">
-				<div class="hamburger-box">
-					<div class="hamburger-inner"></div>
-				</div>
-			</div>
-		</a>
-		<!-- /btn_mobile-->
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-3 col-6">
-					<div id="logo">
-						<a href="index.html" title="Findoctor"><img src="<?= base_url() ?>assets/modal/img/logo.png" data-retina="true" alt="" width="163" height="36"></a>
-					</div>
-				</div>
-				<div class="col-lg-9 col-6">
-					<ul id="top_access">
-						<li><a href="login.html"><i class="pe-7s-user"></i></a></li>
-						<li><a href="register-doctor.html"><i class="pe-7s-add-user"></i></a></li>
-					</ul>
-					<nav id="menu" class="main-menu">
-						<ul>
-							<li>
-								<span><a href="#0">Home</a></span>
-								<ul>
-									<li><a href="index.html">Home Default</a></li>
-									<li><a href="index-2.html">Home Version 2</a></li>
-									<li><a href="index-3.html">Home Version 3</a></li>
-									<li><a href="index-4.html">Home Version 4</a></li>
-                                    <li><a href="index-6.html">Revolution Slider</a></li>
-									<li><a href="index-5.html">With Cookie Bar (EU law)</a></li>
-								</ul>
-							</li>
-							<li>
-								<span><a href="#0">Pages</a></span>
-								<ul>
-									<li><a href="list.html">List page</a></li>
-									<li><a href="grid-list.html">List grid page</a></li>
-									<li><a href="list-map.html">List map page</a></li>
-									<li><a href="detail-page.html">Detail page</a></li>
-									<li><a href="detail-page-2.html">Detail page 2</a></li>
-                                    <li><a href="detail-page-3.html">Detail page 3</a></li>
-									<li><a href="blog-1.html">Blog</a></li>
-									<li><a href="badges.html">Badges</a></li>
-									<li><a href="login.html">Login</a></li>
-									<li><a href="login-2.html">Login 2</a></li>
-									<li><a href="register-doctor.html">Register Doctor</a></li>
-									<li><a href="register.html">Register</a></li>
-									<li><a href="contacts.html">Contacts</a></li>
-								</ul>
-							</li>
-							<li>
-								<span><a href="#0">Extra Elements</a></span>
-								<ul>
-                                	<li><a href="detail-page-working-booking.html">Detail working booking</a></li>
-                                    <li><a href="booking-page.html">Booking page</a></li>
-                                    <li><a href="confirm.html">Confirm page</a></li>
-                                	<li><a href="faq.html">Faq page</a></li>
-                                    <li><a href="coming_soon/index.html">Coming soon</a></li>
-                                     <li><a href="pricing-tables.html">Responsive pricing tables</a></li>
-                                     <li><a href="pricing-tables-2.html">Responsive pricing tables 2</a></li>
-                                     <li><a href="register-doctor-working.html">Working doctor register</a></li>
-									<li><a href="icon-pack-1.html">Icon pack 1</a></li>
-									<li><a href="icon-pack-2.html">Icon pack 2</a></li>
-									<li><a href="icon-pack-3.html">Icon pack 3</a></li>
-									<li><a href="404.html">404 page</a></li>
-								</ul>
-							</li>
-							<li><span><a href="<?= base_url() ?>menu_2/index.html">Menu V2</a></span></li>
-							<li><span><a href="<?= base_url() ?>admin_section/index.html" target="_blank">Admin</a></span></li>
-							<li><span><a href="https://themeforest.net/item/findoctor-doctors-directory-and-book-online-template/20876478?ref=ansonika">Buy this template</a></span></li>
-						</ul>
-					</nav>
-					<!-- /main-menu -->
-				</div>
-			</div>
-		</div>
-		<!-- /container -->
-	</header>
-	<!-- /header -->
-	
-	<main>
+<div id="preloader" class="Fixed">
+    <div data-loader="circle-side"></div>
+</div>
+<div id="page">
+    <header class="header_sticky">
+        <a href="#menu" class="btn_mobile">
+            <div class="hamburger hamburger--spin" id="hamburger">
+                <div class="hamburger-box">
+                    <div class="hamburger-inner"></div>
+                </div>
+            </div>
+        </a>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <div id="logo">
+                        <a href="index.html" title="Findoctor">
+                            <img src="<?= base_url() ?>assets/modal/img/logo.png" width="163" height="36">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-9 col-6">
+                    <ul id="top_access">
+                        <li><a href=""><i class="pe-7s-user"></i></a></li>
+                        <li><a href=""><i class="pe-7s-add-user"></i></a></li>
+                    </ul>
+                    <nav id="menu" class="main-menu">
+                        <ul>
+                            <li><span><a href="">Beranda</a></span></li>
+                            <li><span><a href="">Tentang</a></span></li>
+                            <li><span><a href="">Galeri</a></span></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
--- application/views/v_login.php
+++ application/views/v_login.php
@@ -1,20 +1,28 @@
 <?php include_once('header.php') ?>
-<style>
-        .pad{
-            margin-top: 10px;
-            text-align: center;
-        }
-    </style>
-    <center>
-        <div class="col-lg-4" style="margin-top: 80px">
-            <div class="jumbotron">
-                <form method="POST" action="<?= base_url('login/login_action'); ?>">
-                    <input class="form-control pad" type="text" name="username" id="username" placeholder="Username" />
-                    <input class="form-control pad" type="password" name="pass" id="pass" placeholder="Password" />
-                    <button class="btn btn-primary pad" type="submit">LOGIN </button>
-                </form>
+    <div class="bg_color_2">
+        <div class="container margin_60_35">
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4">
+                    <div class="card text-center">
+                        <h3 class="card-header">Masuk</h3>
+                        <form method="POST" action="<?= base_url('login/login_action'); ?>">
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" name="username" placeholder="Username">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control" name="pass" placeholder="Password">
+                                </div>
+                                <div class="form-group">
+                                    <input class="btn_1" type="submit" value="Masuk">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-4"></div>
             </div>
         </div>
-    </center>
-<!-- Script -->
+    </div>
 <?php include_once('footer.php') ?>
