936a0cbaa65ca15f1f4731051275bfd4820337b8
dytra <vancielnugraha@gmail.com>
2018-05-06 16:41:39 +0700

    admin logout

--- application/views/admin_footer.php
+++ application/views/admin_footer.php
@@ -26,7 +26,7 @@
           <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
           <div class="modal-footer">
             <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-            <a class="btn btn-primary" href="login.html">Logout</a>
+            <a class="btn btn-primary" href="<?php base_url() ?>../Login/logout">Logout</a>
           </div>
         </div>
       </div>
