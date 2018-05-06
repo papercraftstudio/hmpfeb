f35ee336ad00e084800fd8a9b079a524750d838d
dytra <vancielnugraha@gmail.com>
2018-05-06 16:41:28 +0700

    add new logo

--- application/views/admin_header.php
+++ application/views/admin_header.php
@@ -34,7 +34,7 @@
 <body class="fixed-nav sticky-footer" id="page-top">
   <!-- Navigation-->
   <nav class="navbar navbar-expand-lg navbar-dark bg-default fixed-top" id="mainNav">
-    <a class="navbar-brand" href="index.html"><img src="<?= base_url(); ?>assets/modal/admin_section/img/logo.png" data-retina="true" alt="" width="163" height="36"></a>
+    <a class="navbar-brand" href="index.html"><img src="<?= base_url(); ?>assets/modal/admin_section/img/logo2.png" data-retina="true" alt="" width="163" height="36"></a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -127,7 +127,7 @@
           </form>
         </li> -->
         <li class="nav-item">
-          <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
+          <a class="nav-link" data-toggle="modal" data-target="#exampleModal" href="">
             <i class="fa fa-fw fa-sign-out"></i>Logout</a>
         </li>
       </ul>
