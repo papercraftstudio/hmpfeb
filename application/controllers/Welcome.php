265f8011e444a023571a10886349074913695e64
dytra <vancielnugraha@gmail.com>
2018-05-07 21:07:15 +0700

    add home page

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('header');
+		$this->load->view('home');
+		$this->load->view('footer');
 	}
 }
