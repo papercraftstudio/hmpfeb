b2208640c5c81bf6e1e9a210f21d91d3ff5969a5
dytra <vancielnugraha@gmail.com>
2018-05-13 10:53:11 +0700

    add page controller

---
+++ application/controllers/Page.php
@@ -0,0 +1,26 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Page extends MY_Controller {
+
+  function __construct()
+  {
+    parent::__construct([
+            parent::MIDDLEWARE['GUEST'],
+            parent::MIDDLEWARE['USER'],
+            parent::MIDDLEWARE['ADMIN']
+        ]);
+    	$this->load->model('M_Page');
+   }
+
+   function about_us() {
+   		$this->data['guest'] = $this->M_Page->get_page(1);
+   		$this->load->view('single-page',$this->data);
+   }
+
+   function visi_misi() {
+   		$data['guest'] = $this->M_Page->get_page(1);
+   		$this->load->view('single-page',$data);
+   }
+}
