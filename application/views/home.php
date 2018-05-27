22518f212d0533e9120d70ddf680e89d0f70ec97
Yafia Samuel Halim <yafiasamuelhalim@gmail.com>
2018-05-27 23:38:30 +0700

    home slideshow

--- application/views/home.php
+++ application/views/home.php
@@ -1,14 +1,16 @@
+<?php $GLOBALS['guest'] = $guest ?>
+
 <?php function main() { ?>
-    <div class="hero_home version_3">
-        <div class="content">
-            <h3>Ekonomi</h3>
-            <p>
-                Merupakan salah satu ilmu sosial yang mempelajari aktivitas manusia
-                yang berhubungan dengan produksi, distribusi, dan konsumsi terhadap barang dan jasa.
-            </p>
-            <a href="" class="btn_1 medium">Lihat Kegiatan Kami</a>
+<?php $guest = $GLOBALS['guest'] ?>
+    <div class="container">
+            <center>
+            <div id="slides">
+                <?php foreach ($guest as $banner) { ?>
+                    <img src="<?= base_url(); ?>uploads/<?= $banner->file_name;?>">
+                <?php } ?>
+            </div>
+                </center>
         </div>
-    </div>
 <?php } ?>
 
 <?php require_once 'layout/landing/index.php' ?>
